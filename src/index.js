72a087385e00f97163c93a404cdaece81860ed8b
liaozhen <liaoz@mingyuanyun.com>
2022-08-15 15:50:11 +0800

    feat: 重写react.js 完成渲染部分

--- src/index.js
+++ src/index.js
@@ -20,7 +20,6 @@ console.log(element);
 // 所谓的渲染就是按照react元素所描述的结构，创建真实DOM元素，并插入root容器内
 // // 会有ReactDOM来确保浏览器的真实DOM和虚拟DOM一致
 // ReactDOM.render(element, document.getElementById("root"));
-debugger;
 ReactDOM.render(element, document.getElementById("root"));
 
 /** 虚拟dom
--- src/react-dom1.js
+++ src/react-dom1.js
@@ -2,13 +2,13 @@ import { REACT_TEXT } from "./constants";
 
 const render = (vdom, container) => {
   const dom = createDOM(vdom);
-  console.log(123);
-  console.log(dom);
   container.appendChild(dom);
 };
 
 const createDOM = (vdom) => {
   const { type, props } = vdom;
+  console.log("props");
+  console.log(props);
   let dom;
   if (type === REACT_TEXT) {
     dom = document.createTextNode(props.content);
@@ -17,15 +17,42 @@ const createDOM = (vdom) => {
   } else {
     dom = document.createElement(type);
   }
+  if (props) {
+    updateProps(dom, {}, props);
+    if (typeof props.children === "object" && props.children.type) {
+      // 是一个对象且只有一个儿子
+      render(props.children, dom);
+    } else if (Array.isArray(props.children)) {
+      reconcileChildren(props.children, dom);
+    }
+  }
   return dom;
+};
+
+const reconcileChildren = (childrenVdom, parentDOM) => {
+  for (let i = 0; i < childrenVdom.length; i++) {
+    render(childrenVdom[i], parentDOM);
+  }
+};
+
+const updateProps = (dom, oldProps, newProps) => {
+  for (let key in newProps) {
+    if (key === "children") {
+      continue;
+    }
+    if (key === "style") {
+      let styleObj = newProps[key];
+      for (let attr in styleObj) {
+        dom.style[attr] = styleObj[attr];
+      }
+    }
+    dom[key] = newProps[key];
+  }
 };
 
 const mountFunctionComponent = (vdom) => {
   const { type, props } = vdom;
   const renderVdom = type(props);
-  console.log(renderVdom);
-  console.log("函数组件的真实dom");
-  console.log(createDOM(renderVdom));
   return createDOM(renderVdom);
 };
 
